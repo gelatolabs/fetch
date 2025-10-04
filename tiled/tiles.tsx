31bc94f406f5cead49799c3f506b905fa0fa6ec9
Keefer Kosciuch <mail@krourke.org>
2025-10-04 13:26:29 -0400

    Make the map prettier

--- tiled/tiles.tsx
+++ tiled/tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="16" tileheight="16" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="16" tileheight="16" tilecount="19" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../tiles/green.png" width="16" height="16"/>
@@ -12,5 +12,53 @@
    <property name="collides" type="bool" value="true"/>
   </properties>
   <image source="../tiles/wall.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="../tiles/brown.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../tiles/bush.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../tiles/dirt.png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../tiles/flower_blue.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../tiles/flower_yellow.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../tiles/flower.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../tiles/grass_clump.png" width="16" height="16"/>
+ </tile>
+ <tile id="10">
+  <image source="../tiles/green.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../tiles/mushroom.png" width="16" height="16"/>
+ </tile>
+ <tile id="12">
+  <image source="../tiles/path.png" width="16" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../tiles/rock_small.png" width="16" height="16"/>
+ </tile>
+ <tile id="14">
+  <image source="../tiles/rock.png" width="16" height="16"/>
+ </tile>
+ <tile id="15">
+  <image source="../tiles/sand.png" width="16" height="16"/>
+ </tile>
+ <tile id="16">
+  <image source="../tiles/tree.png" width="16" height="16"/>
+ </tile>
+ <tile id="17">
+  <image source="../tiles/wall.png" width="16" height="16"/>
+ </tile>
+ <tile id="18">
+  <image source="../tiles/water.png" width="16" height="16"/>
  </tile>
 </tileset>
