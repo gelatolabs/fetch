2c8e1adcb17d38a9ce9fc3c7f8fd008d5337ea2d
Kyle Farwell <m@kfarwell.org>
2025-10-04 11:09:59 -0400

    initial commit

---
+++ quests.lua
@@ -0,0 +1,96 @@
+local quests = {}
+
+-- Quest Chain 1: Lost Cat
+quests.npcs = {
+    {
+        id = "npc_cat_owner",
+        x = 200,
+        y = 200,
+        size = 16,
+        name = "Old Lady",
+        questId = "quest_lost_cat",
+        isQuestGiver = true
+    },
+    {
+        id = "npc_cat_finder",
+        x = 600,
+        y = 200,
+        size = 16,
+        name = "Child",
+        givesItem = "item_cat",
+        requiresDialog = true
+    },
+    -- Quest Chain 2: Missing Book
+    {
+        id = "npc_librarian",
+        x = 350,
+        y = 450,
+        size = 16,
+        name = "Librarian",
+        questId = "quest_missing_book",
+        isQuestGiver = true
+    },
+    {
+        id = "npc_reader",
+        x = 150,
+        y = 450,
+        size = 16,
+        name = "Reader",
+        givesItem = "item_book",
+        requiresDialog = true
+    },
+    -- Quest Chain 3: Delivery Package
+    {
+        id = "npc_merchant",
+        x = 500,
+        y = 350,
+        size = 16,
+        name = "Merchant",
+        questId = "quest_delivery",
+        isQuestGiver = true
+    },
+    {
+        id = "npc_courier",
+        x = 250,
+        y = 150,
+        size = 16,
+        name = "Courier",
+        givesItem = "item_package",
+        requiresDialog = true
+    }
+}
+
+quests.questData = {
+    quest_lost_cat = {
+        id = "quest_lost_cat",
+        name = "Lost Cat",
+        description = "The old lady lost her cat. Find someone who has seen it!",
+        questGiver = "npc_cat_owner",
+        requiredItem = "item_cat",
+        reward = "Thanks for finding my cat!",
+        active = false,
+        completed = false
+    },
+    quest_missing_book = {
+        id = "quest_missing_book",
+        name = "Missing Book",
+        description = "The librarian needs a rare book returned. Someone borrowed it!",
+        questGiver = "npc_librarian",
+        requiredItem = "item_book",
+        reward = "Thank you for returning the book!",
+        active = false,
+        completed = false
+    },
+    quest_delivery = {
+        id = "quest_delivery",
+        name = "Package Delivery",
+        description = "The merchant has a package that needs picking up from the courier!",
+        questGiver = "npc_merchant",
+        requiredItem = "item_package",
+        reward = "Great! Here's your payment!",
+        active = false,
+        completed = false
+    }
+}
+
+return quests
