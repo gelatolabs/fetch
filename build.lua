437031e7aff0db08f8397b46b169a9735fbaadec
Kyle Farwell <m@kfarwell.org>
2025-10-06 22:28:41 -0400

    add love-build config

---
+++ build.lua
@@ -0,0 +1,11 @@
+return {
+  name = 'Go Fetch',
+  developer = 'Gelato Labs',
+  output = 'dist',
+  version = '1.0',
+  love = '11.5',
+  ignore = {'dist'},
+  icon = 'sprites/robot.png',
+  use32bit = false,
+  identifier = 'xyz.gelatolabs.gofetch'
+}
